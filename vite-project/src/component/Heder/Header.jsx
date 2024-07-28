bbbdbdc53757767c80fc5d42367ba1af83dd1777
AmirMahdiAsghari <amirmahdiasghari2022@gmail.com>
2024-07-28 21:52:10 +0330

    add Logo

--- vite-project/src/component/Heder/Header.jsx
+++ vite-project/src/component/Heder/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import LandSup from '../Login and SignUp/LandSup'
-
+import Logo from '../../assets/imge/logoLandin.svg'
 function Header() {
   return (
   <>
   <section id='header'>
-    <div id='Logo'></div>
+  <div id='LoginB'><LandSup/></div>
     <div id='Meno'></div>
-    <div id='LoginB'><LandSup/></div>
+    <div id='Logo'><img src={Logo} alt="" /></div>
   </section>
   </>
   )
